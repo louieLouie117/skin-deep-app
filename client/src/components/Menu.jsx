2540bb0ba6cc440b1ae91ee6990a343544e2857f
Luis Cardona <cardona-luis@outlook.com>
2022-10-06 09:26:16 -0500

    MenueIcons

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
+
+
 const Menu = ({MenuHandler}) => {
+
+
 
 
   return (
@@ -12,16 +16,38 @@ const Menu = ({MenuHandler}) => {
             <a href="https://calendly.com/skindeep-jamie/30min?month=2022-10">Book Your Spot Today</a>
               
             </li>
-            <li>
-            <a href="mailto:skindeep.jamie@gmail.com?subject=&body=Hello, Jamie">Contact Us</a>
-
-            </li>
+          
             {/* <li>About Skin Deep</li> */}
             <li>
               
               <a href="https://www.skindeephealthservices.com/">More Health Services</a>
               </li>
+
+              <li>
+              <a href="#">Contact Us</a>
+
+            </li>   
+
+              <ul className='ContactIcon-Container'>
+                <li> <img src="/img/icons/phoneIcon.png" alt="" /></li>
+
+                <li> 
+                  
+                    <img src="/img/icons/msgIcon.png" alt="" />
+                </li>
+                <li> 
+                  <a href="mailto:skindeep.jamie@gmail.com?subject=&body=Hello, Jamie">
+                    <img src="/img/icons/mailIcon.png" alt="" />
+                  </a>
+                  
+                  </li>
+
+                <li> <img src="/img/icons/instagramIcon.png" alt="" /></li>
+                <li> <img src="/img/icons/facebookIcon.png" alt="" /></li>
+              </ul>
         </ul>
+       
+
     </div>
   )
 }
