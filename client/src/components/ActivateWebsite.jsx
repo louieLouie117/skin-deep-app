819e88d41b29b39fc2493fde44503c8dafb922ba
Luis Cardona <cardona-luis@outlook.com>
2023-01-12 13:25:34 -0600

    ReactivateWebsiteNotice

---
+++ client/src/components/ActivateWebsite.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function ActiveWebsite(props) {
+  return (
+    
+    <div className='sub-container'>
+        <header>
+
+      <h1>Reactivate Website</h1>
+      <p>Your website is no longer active. Please talk to your website advocate.</p>
+      <p>You can activate website hosting if you need to have your website running but do not need all other services. </p>
+      </header>
+      <main>
+
+        <h3>Website Hosting Only $15.99/m</h3>
+     
+
+        <button>
+      <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=R6GJ75CR7BAAY">Activate/Subscribe</a>
+      </button>
+      
+
+        <img src="https://www.paypalobjects.com/digitalassets/c/website/marketing/na/us/logo-center/9_bdg_secured_by_pp_2line.png" border="0" alt="Secured by PayPal" />
+    </main>
+</div>
+  )
+}
+
+
+export default ActiveWebsite
--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import About from '../components/About'
+// import About from '../components/About'
 // import PRPHeader from '../components/PRPHeader'
 import Menu from '../components/Menu'
 // import NavMenu from '../components/NavMenu'
-import PRPServices from '../components/PRPServices'
+// import PRPServices from '../components/PRPServices'
 import LandingPageStyles from '../modules/LandingPageStyles.css'
 
 import { useState } from 'react'
-import HeroBanner from '../components/HeroBanner'
-import Reviews from '../components/Reviews'
-import Footer from '../components/Footer'
-import Promotion from '../components/Promotion'
+// import HeroBanner from '../components/HeroBanner'
+// import Reviews from '../components/Reviews'
+// import Footer from '../components/Footer'
+// import Promotion from '../components/Promotion'
+import ActiveWebsite from '../components/ActivateWebsite';
+
 
 
 
@@ -19,26 +21,28 @@ const LandingPage = props => {
 
   <LandingPageStyles></LandingPageStyles>
 
-  const [mainMenu, setMainMenu] = useState(false);
+  const [mainMenu] = useState(false);
   // const [menuText, setMenuText] = useState(false);
 
 
   const MenuHandler = (e) => {
-    console.log("Nav Handler was called",e)
-    if(mainMenu === false){
-      setMainMenu(true)
-      return
-    }
-    if(mainMenu === true){
-      setMainMenu(false)
+    console.log("Need to reactivate website")
+    // if(mainMenu === false){
+    //   setMainMenu(true)
+    //   return
+    // }
+    // if(mainMenu === true){
+    //   setMainMenu(false)
 
-    }
+    // }
 
   }
 
 
   return (
     <div className='LandingPageContainer'>
+
+
 
         {/* <NavMenu></NavMenu> */}
 
@@ -50,17 +54,18 @@ const LandingPage = props => {
         <div style={{ display: mainMenu ? "grid": "none"}}>
           <Menu MenuHandler={MenuHandler}></Menu>
         </div>
-
+        <ActiveWebsite></ActiveWebsite>
+{/* 
         <HeroBanner></HeroBanner>
 
         <PRPServices></PRPServices>
-        {/* <PRPHeader></PRPHeader> */}
+        <PRPHeader></PRPHeader>
         <Promotion></Promotion>
 
         <Reviews></Reviews>
         <About MenuHandler={MenuHandler}></About>
 
-        <Footer></Footer>
+        <Footer></Footer> */}
     </div>
   )
 }
